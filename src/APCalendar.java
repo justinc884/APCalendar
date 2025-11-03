cdd9eaf7c1b559893eeec107a43ed3e7d948282b
JustinChan <justinc884@nycstudents.net>
2025-11-03 15:33:36 -0500

    I need to create a calendar.

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -43,6 +43,18 @@ public class APCalendar {
         return i;
     }
     private static int dayOfYear(int month, int day, int year) {
+        int x = firstDayOfYear(year);
+        int days = 1;
+        int months = 1;
+        if (month == 2) {
+            days = 28;
+        }
+        if (((month % 2 == 1) && (month < 9)) || ((months % 2 == 0) && (months > 8))) {
+            days = 31;
+        }
+        if ((month % 2 == 0) && (month < 9)) {
+            days = 30;
+        }
         return 0;
     }
     public static int dayOfWeek (int month, int day, int year) {
